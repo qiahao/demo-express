2ffaeb6e4183af171eeeff2f07608ab7be3c4cc5
qiahao <404546074@qq.com>
2018-10-11 14:11:19 +0800

    u

--- controller/user/login.js
+++ controller/user/login.js
@@ -1,9 +1,18 @@
 var serviceUser = require('../../service/user')
-module.exports = function login(req, res) {
+module.exports = function login(req, res, next) {
   var user = req.body
   serviceUser.login(user, (err, results, fields) => {
+    if (err) {
+      next(new Error('出错啦'))
+    }
+    if (results) {
+      res.json(results)
+    } else {
+      next({
+        code: 1,
+        msg: '账号或密码错误'
+      })
+    }
     
-    
-    res.json(results)
   })
 }
--- service/user/login.js
+++ service/user/login.js
@@ -1,16 +1,4 @@
 var daoUser = require('../../dao/user')
 module.exports = function login(user, cb) {
-  daoUser.login(user, (err, results, fields) => {
-    if (err) {
-      next(new Error('出错啦'))
-    }
-    if (results) {
-      cb(err, results, fields)
-    } else {
-      next({
-        code: 1,
-        msg: '账号或密码错误'
-      })
-    }
-  })
+  daoUser.login(user, cb)
 }
